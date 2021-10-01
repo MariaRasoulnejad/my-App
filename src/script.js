601f63f7121d36aa2beb1aa0e2e604bf421bdff1
MariaRasoulnejad <89032334+MariaRasoulnejad@users.noreply.github.com>
2021-10-01 13:32:43 +0330

    changing js and html

---
+++ src/script.js
@@ -0,0 +1,66 @@
+let now = new Date();
+let update = document.querySelector("#updated");
+let hours = now.getHours();
+let minutes = now.getMinutes();
+update.innerHTML = `${hours} : ${minutes}`;
+let calender = document.querySelector("#newDate");
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+let dates = now.getDate();
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+let second = document.querySelector("#newDate");
+second.innerHTML = `${day} , ${dates} ${month}`;
+function displayWeatherCondition(response) {
+  console.log(response);
+  document.querySelector("#chooseCity").innerHTML = response.data.name;
+  document.querySelector("#display").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "96b2b5e5443c7e65bbc05aacca3fdf89";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "96b2b5e5443c7e65bbc05aacca3fdf89";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let searchForm = document.querySelector("#forcastOne");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
